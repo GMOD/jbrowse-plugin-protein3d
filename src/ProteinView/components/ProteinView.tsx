1cbd5316f7cfd0fe2675c4aa3e00914fa6b4d066
Colin <colin.diesh@gmail.com>
2023-10-16 12:56:48 -0400

    Remove unused for now

--- src/ProteinView/components/ProteinView.tsx
+++ src/ProteinView/components/ProteinView.tsx
@@ -8,12 +8,10 @@ import { PluginContext } from 'molstar/lib/mol-plugin/context'
 import { ParamDefinition } from 'molstar/lib/mol-util/param-definition'
 import { createPluginUI } from 'molstar/lib/mol-plugin-ui'
 import { CameraHelperParams } from 'molstar/lib/mol-canvas3d/helper/camera-helper'
-import { StructureSelection } from 'molstar/lib/mol-model/structure'
 
 // locals
 import { ProteinViewModel } from '../model'
 import { loadStructure } from './util'
-import { Script } from 'molstar/lib/mol-script/script'
 import { doesIntersect2 } from '@jbrowse/core/util'
 
 const ProteinView = observer(function ({ model }: { model: ProteinViewModel }) {
@@ -110,54 +108,11 @@ const ProteinView = observer(function ({ model }: { model: ProteinViewModel }) {
     if (!plugin) {
       return
     }
-    // setSelected is assumed to be a "set" function returned by useState
-    // (selected: any[]) => void
-    // plugin.behaviors.interaction.click.subscribe(event => {
-    //   const selections = [
-    //     ...plugin.managers.structure.selection.entries.values(),
-    //   ]
-    //   console.log({ selections })
-    //   // This bit can be customized to record any piece information you want
-    //   const localSelected: any[] = []
-    //   for (const structure of selections) {
-    //     console.log({ structure }, structure.selection)
-    //     if (!structure.structure) {
-    //       continue
-    //     }
-    //     Structure.eachAtomicHierarchyElement(structure.structure, {
-    //       residue: loc => {
-    //         const position = StructureProperties.residue.label_seq_id(loc)
-    //         localSelected.push({ position })
-    //       },
-    //     })
-    //   }
-    //   console.log({ localSelected })
-    // })
+
     plugin.state.data.events.changed.subscribe(() => {
       const clickedPos =
         plugin.state.getSnapshot().structureFocus?.current?.label
 
-      const ligandData =
-        plugin.managers.structure.hierarchy.selection.structures[0]
-          ?.components[0]?.cell.obj?.data
-
-      console.log({ ligandData })
-
-      const data =
-        plugin.managers.structure.hierarchy.current.structures[0]?.cell.obj
-          ?.data
-      console.log({ data })
-
-      if (data) {
-        const selection = Script.getStructureSelection(
-          Q =>
-            Q.struct.generator.atomGroups({
-              'chain-test': Q.core.rel.eq(['B', Q.ammp('label_asym_id')]),
-            }),
-          data,
-        )
-        const loci = StructureSelection.toLociWithSourceUnits(selection)
-      }
       setMouseClickedPosition(clickedPos)
       if (clickedPos) {
         const [root] = clickedPos.split('|')
@@ -199,27 +154,6 @@ const ProteinView = observer(function ({ model }: { model: ProteinViewModel }) {
         }
       }
     })
-
-    //plugin.canvas3dContext?.input.move.subscribe(obj=>console.log({obj}))
-    //plugin.canvas3d?.input.move.subscribe(obj=>console.log({obj},',k2'))
-    //plugin.canvas3d?.input.move.subscribe(obj => {
-    //  const { x, y } = obj
-    //  const pickingId = plugin.canvas3d?.identify(x, y)
-    //  const r =
-    //    plugin.managers.structure.hierarchy.current.structures[0]?.cell
-    //      .obj?.data
-    //  console.log({x,y})
-    //  if (r) {
-    //    const r2 = Structure.toStructureElementLoci(r)
-    //    console.log({ pickingId, r, r2 })
-    //  }
-    //  //todo: Gets the current source data, return:[40.5922,17.4164,12.4629]
-    //  // const getPickingSourceData = getPickingSourceData(pickingId)
-    //  //todo: Finding Associated Data，return :[43.4349,13.9762,16.5152]
-    //  // const associatedData = findAssociatedData(getPickingSourceData)
-    //  // const anotherPickingId = Structure.toStructureElementLoci(associatedData)
-    //  // this.plugin.managers.camera.focusLoci(anotherPickingId)
-    //})
   }, [plugin, mapping, model])
 
   const width = dimensions.width
