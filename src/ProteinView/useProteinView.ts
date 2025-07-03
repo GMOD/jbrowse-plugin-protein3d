d5e8d3f0ad616b9057a909f3d4fe7a58da9a3271
Colin <colin.diesh@gmail.com>
2025-07-03 11:54:48 -0400

    Add geo export plugin

--- src/ProteinView/useProteinView.ts
+++ src/ProteinView/useProteinView.ts
@@ -1,10 +1,16 @@
 import { useEffect, useRef, useState } from 'react'
 
 // molstar
+import { GeometryExport } from 'molstar/lib/extensions/geo-export'
 import { PluginContext } from 'molstar/lib/mol-plugin/context'
+import { PluginSpec } from 'molstar/lib/mol-plugin/spec'
 import { createPluginUI } from 'molstar/lib/mol-plugin-ui'
 import { renderReact18 } from 'molstar/lib/mol-plugin-ui/react18'
 import { DefaultPluginUISpec } from 'molstar/lib/mol-plugin-ui/spec'
+
+export const ExtensionMap = {
+  'geo-export': PluginSpec.Behavior(GeometryExport),
+}
 
 export default function useProteinView({
   showControls,
@@ -25,11 +31,17 @@ export default function useProteinView({
         }
         const d = document.createElement('div')
         parentRef.current.append(d)
+        const defaultSpec = DefaultPluginUISpec()
         p = await createPluginUI({
           target: d,
           render: renderReact18,
           spec: {
             ...DefaultPluginUISpec(),
+            // extensions: Object.keys(ExtensionMap),
+            behaviors: [
+              ...defaultSpec.behaviors,
+              PluginSpec.Behavior(GeometryExport),
+            ],
             layout: {
               initial: {
                 controlsDisplay: 'reactive',
