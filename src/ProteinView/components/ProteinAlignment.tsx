9ee3fd8cc2c82643d21913efcac5299475563add
Colin <colin.diesh@gmail.com>
2025-10-08 21:34:38 -0400

    Auto-scroll to

--- src/ProteinView/components/ProteinAlignment.tsx
+++ src/ProteinView/components/ProteinAlignment.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useCallback } from 'react'
+import React, { useMemo, useCallback, useEffect, useRef } from 'react'
 
 import { Tooltip, Typography } from '@mui/material'
 import { observer } from 'mobx-react'
@@ -47,49 +47,49 @@ const useStyles = makeStyles()({
 })
 
 // Memoized column component - React.memo prevents re-rendering when props don't change
-const AlignmentColumn = React.memo(function AlignmentColumn({
-  item,
-  index,
-  classes,
-  isGap,
-  showHighlight,
-  isHovered,
-  onMouseOver,
-  onClick,
-}: {
-  item: { top: string; middle: string; bottom: string }
-  index: number
-  classes: Record<string, string>
-  isGap: boolean
-  showHighlight: boolean
-  isHovered: boolean
-  onMouseOver: (i: number) => void
-  onClick: (i: number) => void
-}) {
-  const className = isHovered
-    ? classes.columnHovered
-    : isGap && showHighlight
-      ? classes.columnGapHighlight
-      : classes.column
+const AlignmentColumn = React.memo(
+  React.forwardRef<
+    HTMLDivElement,
+    {
+      item: { top: string; middle: string; bottom: string }
+      index: number
+      classes: Record<string, string>
+      isGap: boolean
+      showHighlight: boolean
+      isHovered: boolean
+      onMouseOver: (i: number) => void
+      onClick: (i: number) => void
+    }
+  >(function AlignmentColumn(
+    { item, index, classes, isGap, showHighlight, isHovered, onMouseOver, onClick },
+    ref,
+  ) {
+    const className = isHovered
+      ? classes.columnHovered
+      : isGap && showHighlight
+        ? classes.columnGapHighlight
+        : classes.column
 
-  return (
-    <div
-      className={className}
-      onMouseOver={() => onMouseOver(index)}
-      onClick={() => onClick(index)}
-    >
-      <span className={classes.letter}>
-        {item.top === ' ' ? <>&nbsp;</> : item.top}
-      </span>
-      <span className={classes.letter}>
-        {item.middle === ' ' ? <>&nbsp;</> : item.middle}
-      </span>
-      <span className={classes.letter}>
-        {item.bottom === ' ' ? <>&nbsp;</> : item.bottom}
-      </span>
-    </div>
-  )
-})
+    return (
+      <div
+        ref={ref}
+        className={className}
+        onMouseOver={() => onMouseOver(index)}
+        onClick={() => onClick(index)}
+      >
+        <span className={classes.letter}>
+          {item.top === ' ' ? <>&nbsp;</> : item.top}
+        </span>
+        <span className={classes.letter}>
+          {item.middle === ' ' ? <>&nbsp;</> : item.middle}
+        </span>
+        <span className={classes.letter}>
+          {item.bottom === ' ' ? <>&nbsp;</> : item.bottom}
+        </span>
+      </div>
+    )
+  }),
+)
 
 const ProteinAlignment = observer(function ({
   model,
@@ -104,9 +104,29 @@ const ProteinAlignment = observer(function ({
     showHighlight,
   } = model
 
+  // Refs for each column
+  const columnRefs = useRef<(HTMLDivElement | null)[]>([])
+  const containerRef = useRef<HTMLDivElement>(null)
+  const isMouseInContainer = useRef(false)
+
   if (alignmentData.length === 0) {
     return <div>No pairwiseAlignment</div>
   }
+
+  // Scroll to hovered position only if mouse is not in the alignment area
+  useEffect(() => {
+    if (
+      !isMouseInContainer.current &&
+      pairwiseAlignmentHoverPos !== undefined &&
+      columnRefs.current[pairwiseAlignmentHoverPos]
+    ) {
+      columnRefs.current[pairwiseAlignmentHoverPos]?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'center',
+      })
+    }
+  }, [pairwiseAlignmentHoverPos])
 
   // Memoize gapSet so it's only recomputed when alignment changes
   const gapSet = useMemo(() => {
@@ -146,7 +166,12 @@ const ProteinAlignment = observer(function ({
     [model, pairwiseAlignmentToStructurePosition],
   )
 
+  const onMouseEnter = useCallback(() => {
+    isMouseInContainer.current = true
+  }, [])
+
   const onMouseLeave = useCallback(() => {
+    isMouseInContainer.current = false
     model.setHoveredPosition(undefined)
     model.clearHoverGenomeHighlights()
   }, [model])
@@ -158,10 +183,18 @@ const ProteinAlignment = observer(function ({
         Alignment of the protein structure file&apos;s sequence with the
         selected transcript&apos;s sequence. Green is the aligned portion
       </Typography>
-      <div className={classes.pre} onMouseLeave={onMouseLeave}>
+      <div
+        ref={containerRef}
+        className={classes.pre}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
         {alignmentData.map((item, i) => (
           <AlignmentColumn
             key={i}
+            ref={el => {
+              columnRefs.current[i] = el
+            }}
             item={item}
             index={i}
             classes={classes}
